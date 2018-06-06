cdc445e1935ae528209196341960c6cb605d40b8
benjaminkieneDiiage <benjamin.kiene@diiage.org>
2018-06-06 14:10:49 +0200

    test

--- server.js
+++ server.js
@@ -1,71 +1,44 @@
+net = require('net');
 var port = 4242;
-var express = require('express');
-var http = require('http');
-var socket = require('socket-io-server');
-var app = express();
-var server = http.Server(app);
- 
+// Keep track of the chat clients
+var clients = [];
 
-socket.init(server);
-server.listen(port);
- 
-// Listen for incoming events from a client.
-// Note that a web socket client id is required, this id is emitted by the
-// socket server when a client connects.
-socket.on('ws-client-id', 'incoming', function (data) 
-{
-    console.log(data);
-});
- 
-// Emit an event with data to all connected clients except for the
-// client that started the event.
-// Note that a web socket client id is required, this id is emitted by the
-// socket server when a client connects.
-socket.broadcast('ws-client-id', 'emit-client', 
-{
-    data: []
-});
- 
-// Emit an event with data to a specific client.
-// Note that a web socket client id is required, this id is emitted by the
-// socket server when a client connects.
-socket.emit('ws-client-id', 'emit-client', 
-{
-    data: []
-});
- 
-// Emit an event with data to all connected clients.
-socket.emitAll('emit-clients', 
-{
-    data: []
-});
- 
-// Access the socketServer object (socket.io constructor).
-socket.socketServer;
- 
-// Close the socket server connection.
-socket.close();
-// Close the http server connection.
-server.close();
-      
-/*var net = require('net');
-var io = require('socket.io');
-
-var port = 4242;
-//tableau des robots
-var bots = [];
+// Start a TCP Server
 net.createServer(function (socket) {
+
+  // Identify this client
   socket.name = socket.remoteAddress + ":" + socket.remotePort 
-  bots.push(socket);
-  console.log(socket.ip + " est connecté");
 
+  // Put this new client in the list
+  clients.push(socket);
 
+  // Send a nice welcome message and announce
+  socket.write(socket.name + " est connecté");
 
+  // Handle incoming messages from clients.
+  socket.on('data', function (data) 
+  {
+    broadcast(data, socket);
+  });
+
+  // Remove the client from the list when it leaves
   socket.on('end', function () {
-    bots.splice(bots.indexOf(socket), 1);
-    broadcast(socket.name + " est déconnecté.\n");
+    clients.splice(clients.indexOf(socket), 1);
+    broadcast(socket.name + " s'est déconnecté.\n");
   });
   
+  // Send a message to all clients
+  function broadcast(message, sender) {
+    clients.forEach(function (client) {
+      // Don't want to send it to sender
+      if (client === sender) return;
+      client.write(message);
+    });
+    // Log it to the server output too
+    process.stdout.write(message)
+  }
+
 }).listen(port);
 
-console.log("Serveur démarré sur le port "+port);*/
+// Put a friendly message on the terminal of the server.
+console.log("serveur connecté sur le port "+port);
