6c5de1d928ca76b878325250d851b0ed819f52cf
benjaminkieneDiiage <benjamin.kiene@diiage.org>
2018-06-06 11:20:28 +0200

    test

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-var net = require('net');
+/*var net = require('net');
 var JsonSocket = require('json-socket');
 
 var port1 = 4242;
 
 var io = require('socket.io');
+
 var server = net.createServer();
 
 server.listen(port1);
@@ -27,8 +28,8 @@ server.on('connection', function(socket) {
 });
 
 console.log("Serveur démarré sur le port 4242\n");
-
-/*net = require('net');
+*/
+net = require('net');
 
 //tableau des robots
 var bots = [];
@@ -36,22 +37,22 @@ net.createServer(function (socket) {
   socket.name = socket.remoteAddress + ":" + socket.remotePort 
   bots.push(socket);
   socket.write(socket.name + " est connecté");
-  socket.on('data', function (data) {
+  /*socket.on('data', function (data) {
     broadcast(data, socket);
-  });
+  });*/
 
-  socket.on('end', function () {
+  /*socket.on('end', function () {
     bots.splice(bots.indexOf(socket), 1);
     broadcast(socket.name + " est déconnecté.\n");
-  });
+  });*/
   
-  function broadcast(message, sender) {
+ /* function broadcast(message, sender) {
     bots.forEach(function (client) {
       if (client === sender) return;
       client.write(message);
     });
     process.stdout.write(message)
-  }
+  }*/
 }).listen(4242);
 
-console.log("Serveur démarré sur le port 4242\n");*/
+console.log("Serveur démarré sur le port 4242\n");
