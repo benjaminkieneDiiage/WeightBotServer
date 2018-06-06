0b4c54bad12638af5241f154726fe9c120da7ec1
benjaminkieneDiiage <benjamin.kiene@diiage.org>
2018-06-06 14:07:11 +0200

    test

--- server.js
+++ server.js
@@ -1,16 +1,52 @@
 var port = 4242;
+var express = require('express');
 var http = require('http');
-var io = require("socket.io");
-var app = http.createServer();
-app.listen(port);
+var socket = require('socket-io-server');
+var app = express();
+var server = http.Server(app);
+ 
 
-var io = io.listen(app);
-
-io.sockets.on('connection', function (socket) 
+socket.init(server);
+server.listen(port);
+ 
+// Listen for incoming events from a client.
+// Note that a web socket client id is required, this id is emitted by the
+// socket server when a client connects.
+socket.on('ws-client-id', 'incoming', function (data) 
 {
-    socket.emit('connection from '+ socket);
+    console.log(data);
 });
-
+ 
+// Emit an event with data to all connected clients except for the
+// client that started the event.
+// Note that a web socket client id is required, this id is emitted by the
+// socket server when a client connects.
+socket.broadcast('ws-client-id', 'emit-client', 
+{
+    data: []
+});
+ 
+// Emit an event with data to a specific client.
+// Note that a web socket client id is required, this id is emitted by the
+// socket server when a client connects.
+socket.emit('ws-client-id', 'emit-client', 
+{
+    data: []
+});
+ 
+// Emit an event with data to all connected clients.
+socket.emitAll('emit-clients', 
+{
+    data: []
+});
+ 
+// Access the socketServer object (socket.io constructor).
+socket.socketServer;
+ 
+// Close the socket server connection.
+socket.close();
+// Close the http server connection.
+server.close();
       
 /*var net = require('net');
 var io = require('socket.io');
